bf74ca11d5073adf05e29e03906e228b06484258
Arun Bishop <arunleob@umich.edu>
2024-04-27 15:34:02 -0400

    Cleaned up files in .gitignore

--- build/CMakeFiles/cartpole.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cartpole.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/cartpole.dir/src/wrapper.c.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/cartpole.dir/src/wrapper.c.o: /home/arun/PinnZoo/src/wrapper.c \
- /usr/include/stdc-predef.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/M_func.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/C_func.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/forward_dynamics.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/inverse_dynamics.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/kinematics.h \
- /home/arun/PinnZoo/models/cartpole/generated_code/kinematics_jacobian.h
