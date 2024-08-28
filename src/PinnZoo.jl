d62d7c45477f94dd84f850b501281a01fb1cbc4c
Arun Bishop <arunleob@umich.edu>
2024-08-27 21:55:33 -0400

    Fix Nadia bug, repo clean-up

--- src/PinnZoo.jl
+++ src/PinnZoo.jl
@@ -3,10 +3,21 @@ module PinnZoo
     using LinearAlgebra
 
     abstract type PinnZooModel end
-    abstract type Quadruped <: PinnZooModel end
 
     const SHARED_LIBRARY_DIR = joinpath(@__DIR__, "../build")
     const MODEL_DIR = joinpath(@__DIR__, "../models")
+
+    # Dynamics functions (from codegen)
+    include(joinpath(@__DIR__, "dynamics.jl"))
+    
+    # Kinematics functions (from codegen)
+    include(joinpath(@__DIR__, "kinematics.jl"))
+
+    # Quaternion helpers
+    include(joinpath(@__DIR__, "quaternions.jl"))
+
+    # Quadruped specific functions (TODO: some of this can be more general)
+    include(joinpath(@__DIR__, "quadruped.jl"))
 
     # Include model files here
     include(joinpath(MODEL_DIR, "cartpole/cartpole.jl"))
@@ -49,20 +60,23 @@ module PinnZoo
         This currently defaults to all zeros unless overridden and will be wrong if there is a quaternion in the state.
     """
     init_state(model::PinnZooModel) = zeros(model.nx) # Define for each model if desired
-
-    # Dynamics functions
-    include(joinpath(@__DIR__, "dynamics.jl"))
     
-    # Kinematics functions
-    include(joinpath(@__DIR__, "kinematics.jl"))
+    ## Exports
+    # Types
+    export PinnZooModel
+
+    # Code-genned functions
+    export is_floating, zero_state, init_state, randn_state
+    export M_func, C_func, forward_dynamics, forward_dynamics_deriv, inverse_dynamics, inverse_dynamics_deriv
+    export velocity_kinematics, velocity_kinematics_T
+    export kinematics, kinematics_jacobian, kinematics_velocity, kinematics_velocity_jacobian
 
     # Quaternion helpers
-    include(joinpath(@__DIR__, "quaternions.jl"))
     export quat_to_axis_angle, axis_angle_to_quat, quat_conjugate, skew, L_mult,
         R_mult, attitude_jacobian, quat_to_rot
 
     # Quadruped specific functions
-    include(joinpath(@__DIR__, "quadruped.jl"))
+    export Quadruped
     export error_jacobian, error_jacobian_T, apply_Δx, state_error
     export B_func, fix_joint_limits, inverse_kinematics, nearest_ik
 
