8bcac337236d6969e68a7d276dfc70645d6330d4
Arun Bishop <arunleob@umich.edu>
2024-10-09 19:00:52 -0400

    Removed biped model files

--- models/biped/generate.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath("../../src"))
-from symbolic_generator import SymbolicGenerator
-
-symb_gen = SymbolicGenerator('biped.urdf', 
-                             floating = True,
-                             kinematics_bodies=['FL_foot', 'FR_foot'])
-symb_gen.generate()
