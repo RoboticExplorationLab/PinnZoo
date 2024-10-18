506a7d10815596a53421c1ff565289b1d7b4179e
Arun Bishop <arunleob@umich.edu>
2024-10-18 13:36:04 -0400

    Made dynamics and kinematics work on AbstractVectors, fixed bug in runtests.jl

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -1,43 +1,43 @@
 @doc raw""" 
-    M_func(model::PinnZooModel, x::Vector{Float64})
+    M_func(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the mass matrix of the model as a function of the configuration (x[1:model.nq])
 """
-function M_func(model::PinnZooModel, x::Vector{Float64})
+function M_func(model::PinnZooModel, x::AbstractVector{Float64})
     M = zeros(model.nv, model.nv)
     ccall(model.M_func_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, M)
     return M
 end
 
 @doc raw"""
-    C_func(model::PinnZooModel, x::Vector{Float64})
+    C_func(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the dynamics bias of the model (coriolos + centrifugal + gravitational forces)
 """
-function C_func(model::PinnZooModel, x::Vector{Float64})
+function C_func(model::PinnZooModel, x::AbstractVector{Float64})
     C = zeros(model.nv)
     ccall(model.C_func_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, C)
     return C
 end
 
 @doc raw"""
-    forward_dynamics(model::PinnZooModel, x::Vector{Float64}, τ::Vector{Float64})
+    forward_dynamics(model::PinnZooModel, x::AbstractVector{Float64}, τ::AbstractVector{Float64})
 
 Return the forward dynamics v̇ = M(x) \ (τ - C) where M is the mass matrix and C is the
 dynamics bias vector.
 """
-function forward_dynamics(model::PinnZooModel, x::Vector{Float64}, τ::Vector{Float64})
+function forward_dynamics(model::PinnZooModel, x::AbstractVector{Float64}, τ::AbstractVector{Float64})
     v̇ = zeros(model.nv)
     ccall(model.forward_dynamics_ptr, Cvoid, (Ptr{Cdouble}, Ptr{Cdouble}, Ref{Cdouble}), x, τ, v̇)
     return v̇
 end
 
 @doc raw"""
-    forward_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, τ::Vector{Float64})
+    forward_dynamics_deriv(model::PinnZooModel, x::AbstractVector{Float64}, τ::AbstractVector{Float64})
 
 Return a tuple of derivatives of the forward dynamics (v̇) with respect to x and τ
 """
-function forward_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, τ::Vector{Float64})
+function forward_dynamics_deriv(model::PinnZooModel, x::AbstractVector{Float64}, τ::AbstractVector{Float64})
     dv̇_dx, dv̇_dτ = zeros(model.nv, model.nx), zeros(model.nv, model.nv)
     ccall(model.forward_dynamics_deriv_ptr, Cvoid, (Ptr{Cdouble}, Ptr{Cdouble}, Ref{Cdouble}, Ref{Cdouble}), 
             x, τ, dv̇_dx, dv̇_dτ)
@@ -45,12 +45,12 @@ function forward_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, τ::Vec
 end
 
 @doc raw"""
-    inverse_dynamics(model::PinnZooModel, x::Vector{Float64}, v̇::Vector{Float64})
+    inverse_dynamics(model::PinnZooModel, x::AbstractVector{Float64}, v̇::AbstractVector{Float64})
 
 Return the inverse dynamics τ = M⩒ + C where M is the mass matrix and C is the dynamics
 bias vector
 """
-function inverse_dynamics(model::PinnZooModel, x::Vector{Float64}, v̇::Vector{Float64})
+function inverse_dynamics(model::PinnZooModel, x::AbstractVector{Float64}, v̇::AbstractVector{Float64})
     τ = zeros(model.nv)
     ccall(model.inverse_dynamics_ptr, Cvoid, (Ptr{Cdouble}, Ptr{Cdouble}, 
             Ref{Cdouble}), x, v̇, τ)
@@ -58,11 +58,11 @@ function inverse_dynamics(model::PinnZooModel, x::Vector{Float64}, v̇::Vector{F
 end
 
 @doc raw"""
-    inverse_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, v̇::Vector{Float64}
+    inverse_dynamics_deriv(model::PinnZooModel, x::AbstractVector{Float64}, v̇::AbstractVector{Float64}
 
 Return a tuple of derivatives of the inverse dynamics (τ) with respect to x and v̇ 
 """
-function inverse_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, v̇::Vector{Float64})
+function inverse_dynamics_deriv(model::PinnZooModel, x::AbstractVector{Float64}, v̇::AbstractVector{Float64})
     dτ_dx, dτ_dv̇ = zeros(model.nv, model.nx), zeros(model.nv, model.nv)
     ccall(model.inverse_dynamics_deriv_ptr, Cvoid, (Ptr{Cdouble}, Ptr{Cdouble}, 
             Ref{Cdouble}, Ref{Cdouble}), x, v̇, dτ_dx, dτ_dv̇)
@@ -70,24 +70,24 @@ function inverse_dynamics_deriv(model::PinnZooModel, x::Vector{Float64}, v̇::Ve
 end
 
 @doc raw"""
-    velocity_kinematics(model::PinnZooModel, x::Vector{Float64})
+    velocity_kinematics(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the matrix E mapping v to q̇, i.e. q̇ = E(q)v, where q = x[1:model.nq]. This mapping is exact,
 i.e. E_T*E = I where E_T comes from velocity_kinematics_T. For an explanation for why Eᵀ != E_T refer to TODO
 """
-function velocity_kinematics(model::PinnZooModel, x::Vector{Float64})
+function velocity_kinematics(model::PinnZooModel, x::AbstractVector{Float64})
     E = zeros(model.nq, model.nv)
     ccall(model.velocity_kinematics_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, E)
     return E
 end
 
 @doc raw"""
-    velocity_kinematics_T(model::PinnZooModel, x::Vector{Float64})
+    velocity_kinematics_T(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the matrix E_T projecting q̇ onto v, i.e. v = E_T(q)q̇, where q = x[1:model.nq]. This respects the
 tangent space structure. For a further explanation, as well as details on why E_T != Eᵀ refer to TODO
 """
-function velocity_kinematics_T(model::PinnZooModel, x::Vector{Float64})
+function velocity_kinematics_T(model::PinnZooModel, x::AbstractVector{Float64})
     E_T = zeros(model.nv, model.nq)
     ccall(model.velocity_kinematics_T_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, E_T)
     return E_T
--- src/kinematics.jl
+++ src/kinematics.jl
@@ -9,47 +9,47 @@ kinematics_size(model::PinnZooModel, ::Val{:Quaternion}) = 7*length(model.kinema
 kinematics_size(model::PinnZooModel, ::Val{:AxisAngle}) = 6*length(model.kinematics_bodies)
 
 @doc raw"""
-    kinematics(model::PinnZooModel, x::Vector{Float64})
+    kinematics(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return a list of the locations of each body in model.kinematics_bodies in the world frame.
 """
-function kinematics(model::PinnZooModel, x::Vector{Float64})
+function kinematics(model::PinnZooModel, x::AbstractVector{Float64})
     locs = zeros(kinematics_size(model))
     ccall(model.kinematics_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, locs)
     return locs
 end
 
 @doc raw"""
-    kinematics_jacobian(model::PinnZooModel, x::Vector{Float64})
+    kinematics_jacobian(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the jacobian of the kinematics function with respect to x (not projected into the tangent space).
 """
-function kinematics_jacobian(model::PinnZooModel, x::Vector{Float64})
+function kinematics_jacobian(model::PinnZooModel, x::AbstractVector{Float64})
     J = zeros(kinematics_size(model), model.nx)
     ccall(model.kinematics_jacobian_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, J)
     return J
 end
 
 @doc raw"""
-    kinematics_velocity(model::PinnZooModel, x::Vector{Float64})
+    kinematics_velocity(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return a list of the instantaneous linear velocities of each body in model.kinematics_bodies in the world frame.
 """
-function kinematics_velocity(model::PinnZooModel, x::Vector{Float64})
+function kinematics_velocity(model::PinnZooModel, x::AbstractVector{Float64})
     locs_dot = zeros(kinematics_size(model))
     ccall(model.kinematics_velocity_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, locs_dot)
     return locs_dot
 end
 
 @doc raw"""
-    kinematics_velocity_jacobian(model::PinnZooModel, x::Vector{Float64})
+    kinematics_velocity_jacobian(model::PinnZooModel, x::AbstractVector{Float64})
 
 Return the jacobian of the kinematics\_velocity function with respect to x (not in the tangent space). If $$J_q$$ is the
 derivative of the kinematics with respect to $$q$$, this jacobian $$J$$ = [$$\dot{J_q}$$ $$J_q$$] where $$\dot{J_q} = \frac{\partial}{\partial q} J_qv$$ and $$J_qv$$ is
 equal to kinematics\_velocity. This also means that $$J\dot{x}$$ expresses the constraint at the acceleration level, i.e. $$\dot{J_q}\dot{q} + J_q\dot{v} = 0$$
 
 """
-function kinematics_velocity_jacobian(model::PinnZooModel, x::Vector{Float64})
+function kinematics_velocity_jacobian(model::PinnZooModel, x::AbstractVector{Float64})
     J_dot = zeros(kinematics_size(model), model.nx)
     ccall(model.kinematics_velocity_jacobian_ptr, Cvoid, (Ptr{Cdouble}, Ref{Cdouble}), x, J_dot)
     return J_dot
