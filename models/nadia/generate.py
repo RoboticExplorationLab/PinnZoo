846ffc7097313bdcc0aa7c8bea23cecb64c04be9
Arun Bishop <arunleob@umich.edu>
2024-11-03 11:39:49 -0500

    Added fixed arm Nadia model

--- models/nadia/generate.py
+++ models/nadia/generate.py
@@ -2,6 +2,7 @@ import os, sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.abspath("../../src"))
 from symbolic_generator import SymbolicGenerator, KinematicsOrientation
+import math
 
 symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.cycloidArms_mj.urdf', 
                              floating = True,
@@ -29,4 +30,11 @@ symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.cycloidArms_mj.urdf
                              kinematics_ori = KinematicsOrientation.Quaternion,
                              gen_dir="./generated_code/simple_1cp_quat",
                              actuated_dofs = slice(6,29))
+symb_gen.generate()
+symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.fixedArms_mj.urdf', 
+                             floating = True,
+                             kinematics_bodies=['L_C', 'R_C'],
+                             kinematics_ori = KinematicsOrientation.AxisAngle,
+                             gen_dir="./generated_code/simple_1cp_aa_no_arms",
+                             actuated_dofs = slice(6,21))
 symb_gen.generate()
