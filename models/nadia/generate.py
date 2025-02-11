97ac4e2454603733b9e96738a6180075333387c4
Quadruped Desktop <cmurexlab@gmail.com>
2025-02-11 15:37:38 -0500

    Changed arms so they aren't straight down, dynamics need to be regenerated

--- models/nadia/generate.py
+++ models/nadia/generate.py
@@ -31,13 +31,13 @@ import math
 #                              gen_dir="./generated_code/simple_1cp_quat",
 #                              actuated_dofs = slice(6,29))
 # symb_gen.generate()
-# symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.fixedArms_mj.urdf', 
-#                              floating = True,
-#                              kinematics_bodies=['L_C', 'R_C'],
-#                              kinematics_ori = KinematicsOrientation.AxisAngle,
-#                              gen_dir="./generated_code/simple_1cp_aa_no_arms",
-#                              actuated_dofs = slice(6,21))
-# symb_gen.generate()
+symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.fixedArms_mj.urdf', 
+                             floating = True,
+                             kinematics_bodies=['L_C', 'R_C'],
+                             kinematics_ori = KinematicsOrientation.AxisAngle,
+                             gen_dir="./generated_code/simple_1cp_aa_no_arms",
+                             actuated_dofs = slice(6,21))
+symb_gen.generate()
 symb_gen = SymbolicGenerator('nadiaV17.fullRobot.simpleKnees.fixedArms_mj.urdf', 
                              floating = True,
                              kinematics_bodies=['L_FL', 'L_FR', 'L_RL', 'L_RR',
